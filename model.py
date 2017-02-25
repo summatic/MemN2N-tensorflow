29bf9dad9087b04d4baa8a6c09135e28c51ca79b
HanseokJo <summatic@gmail.com>
2017-02-25 16:53:18 +0900

    Add comments about dimensionality

--- model.py
+++ model.py
@@ -25,10 +25,10 @@ class MemN2N(object):
         if not os.path.isdir(self.checkpoint_dir):
             raise Exception(" [!] Directory %s not found" % self.checkpoint_dir)
 
-        self.input = tf.placeholder(tf.float32, [None, self.edim], name="input")
-        self.time = tf.placeholder(tf.int32, [None, self.mem_size], name="time")
-        self.target = tf.placeholder(tf.float32, [self.batch_size, self.nwords], name="target")
-        self.context = tf.placeholder(tf.int32, [self.batch_size, self.mem_size], name="context")
+        self.input = tf.placeholder(tf.float32, [None, self.edim], name="input")  # hidden vector
+        self.time = tf.placeholder(tf.int32, [None, self.mem_size], name="time")  # memory
+        self.target = tf.placeholder(tf.float32, [self.batch_size, self.nwords], name="target")  # answer
+        self.context = tf.placeholder(tf.int32, [self.batch_size, self.mem_size], name="context")  # sentences
 
         self.hid = []
         self.hid.append(self.input)
@@ -48,8 +48,8 @@ class MemN2N(object):
     def build_memory(self):
         self.global_step = tf.Variable(0, name="global_step")
 
-        self.A = tf.Variable(tf.random_normal([self.nwords, self.edim], stddev=self.init_std))
-        self.B = tf.Variable(tf.random_normal([self.nwords, self.edim], stddev=self.init_std))
+        self.A = tf.Variable(tf.random_normal([self.nwords, self.edim], stddev=self.init_std))  # A \in R^{d x V}
+        self.B = tf.Variable(tf.random_normal([self.nwords, self.edim], stddev=self.init_std))  # B \in R^{d x V
         self.C = tf.Variable(tf.random_normal([self.edim, self.edim], stddev=self.init_std))
 
         # Temporal Encoding
@@ -59,25 +59,25 @@ class MemN2N(object):
         # m_i = sum A_ij * x_ij + T_A_i
         Ain_c = tf.nn.embedding_lookup(self.A, self.context)
         Ain_t = tf.nn.embedding_lookup(self.T_A, self.time)
-        Ain = tf.add(Ain_c, Ain_t)
+        Ain = tf.add(Ain_c, Ain_t)  # Ain dim: [batch_size, mem_size, edim]
 
         # c_i = sum B_ij * u + T_B_i
         Bin_c = tf.nn.embedding_lookup(self.B, self.context)
         Bin_t = tf.nn.embedding_lookup(self.T_B, self.time)
-        Bin = tf.add(Bin_c, Bin_t)
+        Bin = tf.add(Bin_c, Bin_t)  # Bin dim: [batch_size, mem_size, edim]
 
         for h in xrange(self.nhop):
-            self.hid3dim = tf.reshape(self.hid[-1], [-1, 1, self.edim])
-            Aout = tf.batch_matmul(self.hid3dim, Ain, adj_y=True)
-            Aout2dim = tf.reshape(Aout, [-1, self.mem_size])
-            P = tf.nn.softmax(Aout2dim)
+            self.hid3dim = tf.reshape(self.hid[-1], [-1, 1, self.edim])  # dim: [batch_size, 1, edim]
+            Aout = tf.batch_matmul(self.hid3dim, Ain, adj_y=True)  # dim: [batch_size, 1, mem_size]
+            Aout2dim = tf.reshape(Aout, [-1, self.mem_size])  # dim: [batch_size, mem_size]
+            P = tf.nn.softmax(Aout2dim) # dim: [batch_size, mem_size]
 
-            probs3dim = tf.reshape(P, [-1, 1, self.mem_size])
-            Bout = tf.batch_matmul(probs3dim, Bin)
-            Bout2dim = tf.reshape(Bout, [-1, self.edim])
+            probs3dim = tf.reshape(P, [-1, 1, self.mem_size])  # dim: [batch_size, 1, mem_size]
+            Bout = tf.batch_matmul(probs3dim, Bin)  # dim: [batch_size, 1, edim]
+            Bout2dim = tf.reshape(Bout, [-1, self.edim])  # dim: [batch_size, edim]
 
-            Cout = tf.matmul(self.hid[-1], self.C)
-            Dout = tf.add(Cout, Bout2dim)
+            Cout = tf.matmul(self.hid[-1], self.C)  # dim: [batch_size, edim]
+            Dout = tf.add(Cout, Bout2dim)  # dim: [batch_size, edim]
 
             self.share_list[0].append(Cout)
 
@@ -95,7 +95,7 @@ class MemN2N(object):
         self.build_memory()
 
         self.W = tf.Variable(tf.random_normal([self.edim, self.nwords], stddev=self.init_std))
-        z = tf.matmul(self.hid[-1], self.W)
+        z = tf.matmul(self.hid[-1], self.W)  # dim: [nwords]
 
         self.loss = tf.nn.softmax_cross_entropy_with_logits(z, self.target)
 
@@ -105,7 +105,7 @@ class MemN2N(object):
         params = [self.A, self.B, self.C, self.T_A, self.T_B, self.W]
         grads_and_vars = self.opt.compute_gradients(self.loss,params)
         clipped_grads_and_vars = [(tf.clip_by_norm(gv[0], self.max_grad_norm), gv[1]) \
-                                   for gv in grads_and_vars]
+                                   for gv in grads_and_vars]  # gradient clipping
 
         inc = self.global_step.assign_add(1)
         with tf.control_dependencies([inc]):
